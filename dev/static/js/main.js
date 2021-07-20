43c9ab178e72808c2e73a67e8614fcc7c1b541a3
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2021-07-20 15:45:33 +0400

    Закончил верстку страницы О Компании, начал верстку страницы Стоимости пдоставки

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -34,6 +34,19 @@
       });
     }
   };
+  const getGormValidation = () => {
+    const forms = document.querySelectorAll('.need-validate');
+    Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  };
 
-  getMap();
+  // getMap();
+  getGormValidation();
 })();
